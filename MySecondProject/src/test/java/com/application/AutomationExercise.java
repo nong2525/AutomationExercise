f06324257861bd5f2d15012b6200a528e4c4d3e9
Nongnuch <nong.jovi@hotmail.com>
2024-01-23 14:48:37 -0500

    updating new code

--- MySecondProject/src/test/java/com/application/AutomationExercise.java
+++ MySecondProject/src/test/java/com/application/AutomationExercise.java
@@ -1,11 +1,15 @@
 package com.application;
 
 public class AutomationExercise {
+	
+	
+	public void print() {
 
-	public static void main(String[] args) {
+
 	System.out.println("this is my second project");
 	System.out.println("I wont give up I will do it ");
-
 	}
+
+	
 
 }
