c0357403401e849e9348d13ed6e128dedfcd1664
Nongnuch <nong.jovi@hotmail.com>
2024-01-23 12:36:03 -0500

    Add more print statement to the project

--- MySecondProject/src/test/java/com/application/AutomationExercise.java
+++ MySecondProject/src/test/java/com/application/AutomationExercise.java
@@ -4,6 +4,7 @@ public class AutomationExercise {
 
 	public static void main(String[] args) {
 	System.out.println("this is my second project");
+	System.out.println("I wont give up I will do it ");
 
 	}
 
